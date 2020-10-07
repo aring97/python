38d6d1805159603b9fe033cadfa5ec1802986924
Austin Ring <aring97@gmail.com>
2020-10-07 15:20:30 -0500

    finished chapter 5

--- location/request.py
+++ location/request.py
@@ -20,3 +20,10 @@ def get_single_location(id):
         if location["id"]==id:
             request_location=location
     return request_location
+
+def  create_location(location):
+  max_id=LOCATIONS[-1]["id"]
+  new_id=max_id+1
+  location["id"]=new_id
+  LOCATIONS.append(location)
+  return location
